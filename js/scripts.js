6e7f65b769308342ec5f0d81015756db8bceb9a7
AkbarjonTojiyev <tojiyev.akbarjon@gmail.com>
2020-11-12 15:22:23 +0500

    Refactor session

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,21 @@ var elForm = document.querySelector('.form');
 var elInput = elForm.querySelector('.form-control');
 var effort = 1;
 var passKey = 123;
-
+var entered = false;
 
 elForm.addEventListener('submit', function(evt) {
 	evt.preventDefault();
-
+	if(entered) {
+		alert('Siz tizimdasiz )');
+		return;
+	}
 	if(effort < 5) {
 		var getPass = parseInt(elInput.value.trim(), 10);
 
 		if(getPass === passKey) {
 			document.body.classList.add('bg-success');
 			alert('Xush kelibsiz!');
+			entered = true;
 		} else {
 			alert('Parol xato terildi!');
 		}
